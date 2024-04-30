6dc5731967b7671a1d108d4fffdd01c959da1aa5
ahnaf4d <ahnaftahmid802@gmail.com>
2024-04-30 16:42:44 +0600

    json data aded

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from '../About/About';
 import Carousel from '../Carousel/Carousel';
+import Services from '../Services/Services';
 
 const Home = () => {
   return (
     <div>
       <Carousel></Carousel>
       <About></About>
+      <Services></Services>
     </div>
   );
 };
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,16 @@
+const Services = () => {
+  return (
+    <div className='space-y-6'>
+      <article className='text-center'>
+        <span className='text-3xl text-orange-500 font-bold'>Service</span>
+        <h1 className='text-5xl font-bold my-3'>Our Service Area</h1>
+        <p className='max-w-lg mx-auto mb-4'>
+          The majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default Services;

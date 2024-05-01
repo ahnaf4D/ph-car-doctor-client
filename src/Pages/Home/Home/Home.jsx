51a10249dcd9666f470690d80352c8c88ea5f44c
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-01 10:17:09 +0600

    features and products components added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,7 +1,10 @@
+import Features from '../../Features/Features';
 import About from '../About/About';
 import Carousel from '../Carousel/Carousel';
 import ContactStat from '../ContactStat/ContactStat';
+import Products from '../Products/Products';
 import Services from '../Services/Services';
+import Teams from '../Teams/Teams';
 
 const Home = () => {
   return (
@@ -10,6 +13,9 @@ const Home = () => {
       <About></About>
       <Services></Services>
       <ContactStat></ContactStat>
+      <Products></Products>
+      <Teams></Teams>
+      <Features></Features>
     </div>
   );
 };
